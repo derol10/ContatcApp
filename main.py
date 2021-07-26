6dca2dfac1d7b6878cd8c208469f879fc6044551
derol10 <emmanuel.derol@gmail.com>
2021-07-26 12:49:53 -0400

    Create main.py
    Fichero principal del programa.

---
+++ main.py
@@ -0,0 +1,52 @@
+"""
+Introducción a Programación con Python.
+Proyecto Agenda teléfonica de contactos.
+
+Profesor:
+Francis Fulgencio
+
+Integrantes:
+    Emmanuel Del Rosario Leonardo
+    Josué Rafael Piña Fermín
+    John Albert González De La Rosa
+    Nelis Altagracia Ortiz Melo
+"""
+
+"""
+Trabajo Final
+Usando todos los conocimientos aprendidos durante el curso,
+Realizar uno de los siguientes proyectos: OPCIÓN 1
+
+OPCION 1: Hacer un programa que funcione como agenda telefónica
+    -El programa debe precargar 10 contactos al iniciar.
+    -Al iniciar el programa el usuario debe tener opciones que le permitan agregar, buscar, actualizar y 
+        borrar un contacto. También una opción que le permita listar todos los contactos.
+    -Cada contacto debe tener al menos 3 campos Id, nombre y teléfono. (puede agregar todos los que quiera).
+    -El programa debe permitir buscar por id y por nombre.
+    -Cuando elija agregar contacto debe de pedir si desea agregar otro.
+    -Después de cada opción debe volver al menú.
+    -El programa solo terminara cuando el usuario elija salir.
+"""
+
+"""
+Análisis de los objetos:
+-Base de Datos.
+    -Agregar.
+    -Buscar.
+    -Actualizar.
+    -Borrar.
+    -Guardar/Descartar los cambios
+
+-Intefaz Gráfica.
+    -Pantalla principal.
+        -Cuadro de búsqueda.
+        -My Card.
+        -Lista de Contactos (Grupo de Letras)
+
+    -Buscador Integrado.
+    -Agregar Contacto.
+    -Perfil de Contacto.
+    -Editar Contacto.
+    -Delete Contact (Ventana emergente desde abajo.)
+
+"""
