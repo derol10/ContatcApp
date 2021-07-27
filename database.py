4e9849f69f5e08fb81016982088aff387065e00f
derol10 <emmanuel.derol@gmail.com>
2021-07-27 13:08:41 -0400

    Revision con John, parte 1

--- database.py
+++ database.py
@@ -12,15 +12,20 @@ class QueryBase:
         """
         self.micursor.execute(query)
 
-    def insertarValores (self, nombreTabla, nombre, apellido = "", phone = "", email = ""):
-        query = f"""
-        INSERT INTO {nombreTabla} VALUES ( NULL , ? , ? , ? , ?);
-        """
-        self.micursor.execute(query,(nombre, apellido, phone, email))
+    def insertarValores (self, nombreTabla, nombre = "", apellido = "", phone = "", email = ""):
+        if nombre != "" or apellido != "" or phone != "" or email !="":
+            query = f"""
+            INSERT INTO {nombreTabla} VALUES ( NULL , ? , ? , ? , ?);
+            """
+            self.micursor.execute(query,(nombre, apellido, phone, email))
+            
+            return "Contacto guardado exitosamente."
+        else:
+            return ("Se han descartado los cambios")
 
     def buscarValor (self, nombreTabla, valor):
         query = f"""
-        SELECT contactID, nombre, apellido FROM {nombreTabla}
+        SELECT * FROM {nombreTabla}
         WHERE nombre LIKE ? OR apellido LIKE ? OR phone LIKE ? OR email LIKE ?;
         """
         self.micursor.execute(query,(valor, valor, valor, valor))
@@ -34,5 +39,30 @@ class QueryBase:
         self.micursor.execute(query,( contactID, ))
         return self.micursor.fetchall()
 
+    def buscarParam_byContactID (self, nombreTabla, contactID):
+        query = f"""
+        SELECT * FROM {nombreTabla}
+        WHERE contactID = ?;
+        """
+        self.micursor.execute(query,(contactID, ))
+        return self.micursor.fetchall()
 
+
+
+    def actualizar_contacto(self, nombreTabla, contactID, nombre = "", apellido ="", phone = "", email = ""):
+        
+        abc = self.buscarParam_byContactID(nombreTabla, contactID)
+        listavieja = abc[0]
+        listanueva = (contactID, nombre, apellido, phone, email)
+        parametros = ("concactID", "nombre", "apellido", "phone", "email")
+
+        for i in range(1,5):
+            if listavieja[i] != listanueva[i] and listanueva[i] != "":
+                query= f"""
+                UPDATE {nombreTabla}
+                SET {parametros[i]} = ?
+                WHERE contactID = ? ;
+                """
+                
+                self.micursor.execute(query,(listanueva[i], contactID))
 
--- main.py
+++ main.py
@@ -20,13 +20,31 @@ miTabla = "myContacts"
 myContactApp = QueryBase(miDataBase)
 
 #myContactApp.crearTabla(miTabla)
-#myContactApp.insertarValores(nombreTabla=miTabla, nombre="Maria", apellido="Abreu", phone="809-962-6944", email="maria@gmail.com")
-myContactApp.eliminarcontactID( miTabla, 4)
+#myContactApp.insertarValores(nombreTabla=miTabla, nombre="Josefa", apellido="Martinez", phone="8296361446", email="john.albert@gmail.com")
+#myContactApp.eliminarcontactID( miTabla, 5)
 
+
+myContactApp.actualizar_contacto(miTabla, 11, phone=82923332222, email="peter.cabrera@gmail.com")
+# Búsqueda inteligente
 resultados = myContactApp.buscarValor(miTabla, "%")
+
+# Sección Alfabética
+# resultados = myContactApp.buscarValor(miTabla, "A%")
+# resultados = myContactApp.buscarValor(miTabla, "J%")
 
 myContactApp.conexion.commit()
 myContactApp.micursor.close()
 myContactApp.conexion.close()
 
+
+"""def insertarNumero (numero):
+    try: 
+        myContactApp.conexion.commit()
+        myContactApp.micursor.close()
+        myContactApp.conexion.close()
+    except:
+        print("ERROR: el valor ingresado debe ser un numero")
+"""
+
 print(resultados)
+
